64e47afb9e28d88563d4af453c4ba9eb1e5726f1
Andrew Toader <69984232+aet37@users.noreply.github.com>
2025-08-22 16:23:02 -0400

    Create try_discover_connect.py

---
+++ software/try_discover_connect.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from mbientlab.metawear import MetaWear
+from mbientlab.metawear.cbindings import *
+from mbientlab.warble import *
+from time import sleep
+import platform
+import six
+
+print('Scanning...')
+
+devices = {}
+
+def handler(result):
+	devices[result.mac] = result.name
+
+BleScanner.set_handler(handler)
+BleScanner.start()
+sleep(5.0)
+BleScanner.stop()
+
+Bid_arr = []
+for address, name in six.iteritems(devices):
+	if name =='MetaWear':
+		Bid_arr.append(address)
+
+print(Bid_arr)
